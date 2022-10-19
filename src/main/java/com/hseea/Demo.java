d6fc6f39de61741387c27cfce27059d28e447b8f
huangshun <hseea2015@163.com>
2022-10-19 15:25:12 +0800

    工具类

---
+++ src/main/java/com/hseea/Demo.java
@@ -0,0 +1,12 @@
+package com.hseea;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Demo {
+    public static void main(String[] args) {
+        int [] a = {1, 3};
+        Arrays.sort(a);
+        String s = "";
+    }
+}
